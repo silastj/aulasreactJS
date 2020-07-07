6d1e1be0e0fea44537a1514a628c9e300d38a72f
aSilas <silastj@hotmail.com>
2020-07-07 01:26:53 -0300

    Elementos aninhados

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ console.log(h1);
 
 //IREMOS REDENRIZAR O MEU H1 NA TELA COM O DOM RENDER
 ReactDOM.render(h1, document.getElementById('app'));
+
+//COLOCANDO ELEMENTO DENTRO DO OUTRO NO HTML
+var h2 = React.createElement('h2', null, React.createElement('span', null, "Texto do span"))
+ReactDOM.render(h2, document.getElementById('app1'));
+
+
+
+//RENDERIZAR MAIS DE 1 ELEMENTO e ELEMENTOS ANINHADOS
+var h3 = React.createElement('h3', null, [
+    React.createElement('p', null, "Paragrafo01"),
+    React.createElement('p', null, 'Paragrafo02')
+])
+ReactDOM.render(h3, document.getElementById('app2'));
+
+
+
+
--- readme.txt
+++ readme.txt
@@ -9,9 +9,14 @@ Perfimance( manipulação de DOM);
 
 https://pt-br.reactjs.org/docs/getting-started.html
 
+IREMOS CHAMAR ESSES LINKS ABAIXO O REACT E O REACT DOM
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.13.1/umd/react.production.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.13.1/umd/react-dom.production.min.js"></script>
 
-
+OQ MAIS IREMOS USAR ANBAIXO:
 Iremos mais utilizar é: Componente / CreateClass / CreateElement
+
+
+
 
