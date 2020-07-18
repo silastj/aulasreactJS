6dcfef15c5fe1438b0c3c39dcfc035b4c90d1e70
aSilas <silastj@hotmail.com>
2020-07-18 02:06:05 -0300

    Iniciando a props

---
+++ Modulo01Parte02/readme.txt
@@ -0,0 +1,6 @@
+=> PASSANDO PROPS
+
+Seria um atributo de tag no html
+
+Por exemplo uma class ou um ID
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,41 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const validate = require('webpack-validator')
 
-module.exports = {
-    devtool: 'source-map',
+module.exports = validate({
+  devtool: 'source-map',
 
-    entry:[
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-         path.join(__dirname, 'src', 'index')
-    ],
-    
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
-    },
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
 
-    module: {
-        preLoaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'standard'
-        }],
-        rules: [{
-            test:/\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'babel-loader'
-        }]
-    },
-    performance : {
-        hints : false
-    }      
-}
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
 
-
-
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'babel'
+    }]
+  }
+})
